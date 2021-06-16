84f1dd38b92c3367288f7e41d47015d6d7904867
nacho481 <nachitoatilano@gmail.com>
2021-06-15 22:15:04 -0700

    I count the number of even digits provided with 2 arrays

--- Arrays101/Max_Consecutive_Ones/src/Main.java
+++ Arrays101/Max_Consecutive_Ones/src/Main.java
@@ -1,2 +1,28 @@
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int [] tempArray = {12,345,2,6,7896};
+        int [] tempArray2 = {555,901,482,1771};
+        System.out.println("Temp array no. of even digits: " + findNo(tempArray));
+        System.out.println("Temp array no. of even digits: " + findNo(tempArray2));
+    }
+
+    public static int findNo(int[] nums)
+    {
+        int counter = 0;
+
+        for(int x = 0; x < nums.length; ++x)
+        {
+            int digit = 0;
+            while(nums[x] != 0)
+            {
+                ++digit;
+                nums[x] = nums[x] / 10;
+            }
+            if(digit % 2 == 0)
+                ++counter;
+        }
+        return counter;
+    }
 }
